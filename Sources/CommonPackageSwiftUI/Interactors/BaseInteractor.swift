fd0020f7a70bdc821bb61999a0d4089ce0a487ea
Tan Vo <vhtan.vht@gmail.com>
2023-09-25 13:57:01 +0700

    Commit

--- Sources/CommonPackageSwiftUI/Interactors/BaseInteractor.swift
+++ Sources/CommonPackageSwiftUI/Interactors/BaseInteractor.swift
@@ -9,18 +9,19 @@ import SwiftUI
 import Combine
  
 public protocol BaseInteractor {
-//    func activityIndicator(isLoading: Binding<Bool>)
-//    func handleErrorTracker(errorMessage: Binding<String?>)
-//    var activityIndicator: ActivityIndicator { get set }
-//    var cancelBag: CancelBag { get set }
+    func activityIndicator(isLoading: Binding<Bool>)
+    func handleErrorTracker(errorMessage: Binding<String?>)
+    var activityIndicator: ActivityIndicator { get set }
+    var cancelBag: CancelBag { get set }
+    var errorTracker: ErrorTracker { get set }
 }
 
 public extension BaseInteractor {
-//    func activityIndicator(isLoading: Binding<Bool>) {
-//        activityIndicator.loading
-//            .sink(receiveValue: { flag in
-//                isLoading.wrappedValue = flag
-//            })
-//            .store(in: cancelBag)
-//    }
+    func activityIndicator(isLoading: Binding<Bool>) {
+        activityIndicator.loading
+            .sink(receiveValue: { flag in
+                isLoading.wrappedValue = flag
+            })
+            .store(in: cancelBag)
+    }
 }
