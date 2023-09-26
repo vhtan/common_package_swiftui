ac83682ae3098ea6c30396173745278efc80be09
Tan Vo <vhtan.vht@gmail.com>
2023-09-26 17:21:29 +0700

    Commit

--- Sources/CommonPackageSwiftUI/Helpers/ActivityIndicator.swift
+++ Sources/CommonPackageSwiftUI/Helpers/ActivityIndicator.swift
@@ -8,62 +8,48 @@
 import Foundation
 import Combine
 
-/**
- Enables monitoring of sequence computation.
- If there is at least one sequence computation in progress, `true` will be sent.
- When all activities complete `false` will be sent.
- */
 public final class ActivityIndicator {
-    private struct ActivityToken<Source: Publisher> {
-        let source: Source
-        let beginAction: () -> Void
-        let finishAction: () -> Void
-        
-        func asPublisher() -> AnyPublisher<Source.Output, Source.Failure> {
-            source.handleEvents(receiveCompletion: { _ in
-                finishAction()
-            }, receiveCancel: {
-                finishAction()
-            }, receiveRequest: { _ in
-                beginAction()
-            }).eraseToAnyPublisher()
-        }
-    }
-    
-    @Published private var relay = 0
+    @Published private var count = 0
     private let lock = NSRecursiveLock()
-    
-    public var loading: AnyPublisher<Bool, Never> {
-        $relay.map { $0 > 0 }
-            .removeDuplicates()
-            .eraseToAnyPublisher()
-    }
     
     public init() {}
     
-    func trackActivityOfPublisher<Source: Publisher>(source: Source) -> AnyPublisher<Source.Output, Source.Failure> {
-        return ActivityToken(source: source) {
-            self.increment()
-        } finishAction: {
-            self.decrement()
-        }.asPublisher()
+    var loading: AnyPublisher<Bool, Never> {
+        $count.print("loading")
+            .map({$0 > 0})
+            .print("loading")
+            .eraseToAnyPublisher()
+    }
+    
+    func trackActivity<T: Publisher>(_ source: T) -> AnyPublisher<T.Output, T.Failure> {
+        return source
+            .handleEvents(receiveCompletion: { _ in
+                self.decrement()
+            }, receiveCancel: {
+                self.decrement()
+            }, receiveRequest: { [weak self] _ in
+                self?.increment()
+            })
+            .eraseToAnyPublisher()
     }
     
     private func increment() {
         lock.lock()
-        relay += 1
-        lock.unlock()
+        defer { lock.unlock() }
+        
+        count += 1
     }
     
     private func decrement() {
         lock.lock()
-        relay -= 1
-        lock.unlock()
+        defer { lock.unlock() }
+        
+        count -= 1
     }
 }
 
 public extension Publisher {
     func trackActivity(_ activityIndicator: ActivityIndicator) -> AnyPublisher<Self.Output, Self.Failure> {
-        activityIndicator.trackActivityOfPublisher(source: self)
+        activityIndicator.trackActivity(self)
     }
 }
