857065ba7186a68e1d49f18f2eb6c2aaddc39c14
Tan Vo <vhtan.vht@gmail.com>
2023-12-17 22:11:36 +0700

    change api link

--- lib/config/config_prod.dart
+++ lib/config/config_prod.dart
@@ -2,7 +2,7 @@ import 'package:mvvm_cubit/config/base_config.dart';
 
 class ConfigProd extends BaseConfig {
   @override
-  String get baseUrl => 'https://giamsatdieuquy.acb.vn/vietmap-api-app';
+  String get baseUrl => 'https://fundtransport-app.acb.vn/vietmap-api-app';
 
   @override
   String get vacomUrl =>
