8a6d1de0db401d3ad2ae08ebe064c0bcf60aba5a
Tan Vo <vhtan.vht@gmail.com>
2024-07-03 15:01:43 +0700

    build PAT android: v1.0.59. iOS: v1.0.43+18

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -19,8 +19,7 @@ class Environment {
   // build: flutter build -t lib/main.dart --dart-define=env=stg
   // flutter build apk --dart-define=env=stg
   Future<void> initConfig() async {
-    const environmentParam =
-        String.fromEnvironment('env', defaultValue: 'prod');
+    const environmentParam = String.fromEnvironment('env', defaultValue: 'stg');
     switch (environmentParam) {
       case 'stg':
         environmentConfig = Env.STAG;
